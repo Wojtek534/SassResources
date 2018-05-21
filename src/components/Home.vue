394a68298cea7af5a2832e5e608492f5cd999a4d
wojtek534 <wojtek.rzepkaa@gmail.com>
2018-05-22 00:34:04 +0200

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section-home">
+    <feature-box/>
+    <flipper-card/>
+  </section>
+</template>
+<script>
+import FeatureBox from "../components/content/featureBox/FeatureBox";
+import FlipperCard from "../components/content/flipperCard/FlipperCard";
+export default {
+  components: {
+    FeatureBox,
+    FlipperCard
+  }
+};
+</script>
---
+++ src/components/content/featureBox/FeatureBox.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="row">
+    <div class="col-1-of-4">
+      <div class="feature-box">
+        <i class="material-icons feature-box__icon">
+          monetization_on
+        </i>
+        <h3 class="feature-box__header u-margin-bottom-small">Currency</h3>
+        <p class="feature-box__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rhoncus laoreet elit sit amet luctus. Nam hendrerit volutpat libero, a finibus arcu.
+        </p>
+      </div>
+    </div>
+    <div class="col-1-of-4">
+      <div class="feature-box">
+        <i class="material-icons feature-box__icon">
+          monetization_on
+        </i>
+        <h3 class="feature-box__header u-margin-bottom-small">Currency</h3>
+        <p class="feature-box__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rhoncus laoreet elit sit amet luctus. Nam hendrerit volutpat libero, a finibus arcu.
+        </p>
+      </div>
+    </div>
+    <div class="col-1-of-4">
+      <div class="feature-box">
+        <i class="material-icons feature-box__icon">
+          monetization_on
+        </i>
+        <h3 class="feature-box__header u-margin-bottom-small">Currency</h3>
+        <p class="feature-box__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rhoncus laoreet elit sit amet luctus. Nam hendrerit volutpat libero, a finibus arcu.
+        </p>
+      </div>
+    </div>
+    <div class="col-1-of-4">
+      <div class="feature-box">
+        <i class="material-icons feature-box__icon">
+          monetization_on
+        </i>
+        <h3 class="feature-box__header u-margin-bottom-small">Currency</h3>
+        <p class="feature-box__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rhoncus laoreet elit sit amet luctus. Nam hendrerit volutpat libero, a finibus arcu.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
